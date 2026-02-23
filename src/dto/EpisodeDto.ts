a1e581433d4b1f003ff51c5c6194bb7ada60039d
Darshan Dalwadi <35288602+idarshandalwadi@users.noreply.github.com>
2026-02-23 19:04:08 +0530

    Updated Episode DTO to add Validations
    Added DTO (Request) Validations using class-validator and class-transform package

--- src/dto/EpisodeDto.ts
+++ src/dto/EpisodeDto.ts
@@ -1,15 +1,21 @@
 /* eslint-disable prettier/prettier */
-import { IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsBoolean, IsDate, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateEpisodeDto {
 
   @IsString()
   name: string;
 
-  
+  @IsBoolean()
+  @IsOptional()   // To mark it optional property in input
   featured?: boolean;
 
+  @IsNumber()
   year: number;
 
+  //Need to pass Date as string in input/POST call, so here it will convert that string into Date.
+  @IsDate()
+  @Type(() => Date)
   publishedAt: Date;
 }
